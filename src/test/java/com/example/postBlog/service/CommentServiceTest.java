2a2f5d01869ea41982c4353d5f065f347873a0b2
Amandaakareen <amandakaren526292@gmail.com>
2022-11-19 21:34:43 -0300

    iniciando teste unitario em comentarios

---
+++ src/test/java/com/example/postBlog/service/CommentServiceTest.java
@@ -0,0 +1,59 @@
+package com.example.postBlog.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CommentServiceTest {
+    @Test
+    void testAddComment() {
+        //cenario
+
+        //ação
+
+        //varificação
+
+    }
+
+    @Test
+    void testCheckComment() {
+        //cenario
+
+        //ação
+
+        //varificação
+
+
+    }
+
+    @Test
+    void testDeleteComment() {
+        //cenario
+
+        //ação
+
+        //varificação
+
+
+    }
+
+    @Test
+    void testDeleteListCommentByIdPost() {
+        //cenario
+
+        //ação
+
+        //varificação
+
+
+    }
+
+    @Test
+    void testDeleteListCommentByIdUser() {
+        //cenario
+
+        //ação
+
+        //varificação
+
+
+    }
+}
